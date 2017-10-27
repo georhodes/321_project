94f4a1b5dae270b3a5bdaa152b03dea54b70b86b
George Rhodes <gerhodes@uw.edu>
2017-10-27 11:44:46 -0700

    Includes Lab 8 (incomplete), changes to Scratchpad to include making all variables lowercase. Changes to full.R to subset data.

--- full.R
+++ full.R
@@ -14,22 +14,25 @@ library("tibble")
 load("/Users/george/Documents/School/UW/SOC321/honors_thesis/honors_thesis/NSDUH-2015-survey-data.rda")
 nsduh2015 <- PUF2015_102016
 
+#subset the data
+  subset_nsduh2015 <- nsduh2015 %>%
+  select(txevrrcvd, alclottm, sexage, newrace2, sexrace, eduhighcat, ireduhighst2, al30est, alcus30d, alcbng30d, irpinc3, irfamin3, poverty3, coutyp2, alcwd2sx,alcemopb) 
 
 # Who spent time in drug treatment ever? (TX01)		TXEVRRCVD	1=yes
-recieved_treatment <- nsduh2015 %>%
-  select(Variables/columns you want to view) %>%
-  filter(txevrrcvd == 1)
+#recieved_treatment <- nsduh2015 %>%
+ # select(Variables/columns you want to view) %>%
+  #filter(txevrrcvd == 1)
 
 
 # Find out who spent no time getting or drinking alcohol in past 12 months
 # (DRALC01) ALCLOTTM 
 #83= Did not use or used < 6 dys Log asn 117
 #93= Did not use past 12 mo or <6 dys 11935
-sober12months <-
+#sober12months <-
   
   #spent time in treatment ever AND hasn't drank in last 12 months
   #This would be who is sober today. 
-  sober_after_treatment <- recieved_treatment AND sober12months
+ # sober_after_treatment <- recieved_treatment AND sober12months
 
 
 
--- scratchpad.R
+++ scratchpad.R
@@ -3,3 +3,23 @@
 #How do I import only specific variables with specific values? 
 #How do I avoid the giant data table and only import relevent data? 
 
+# Who spent time in drug treatment ever? (TX01)		TXEVRRCVD	1=yes
+recieved_treatment <- nsduh2015 %>%
+  select(Variables/columns you want to view) %>%
+  filter(txevrrcvd == 1)
+
+
+# Find out who spent no time getting or drinking alcohol in past 12 months
+# (DRALC01) ALCLOTTM 
+#83= Did not use or used < 6 dys Log asn 117
+#93= Did not use past 12 mo or <6 dys 11935
+sober12months <-
+  
+  #spent time in treatment ever AND hasn't drank in last 12 months
+  #This would be who is sober today. 
+  sober_after_treatment <- recieved_treatment AND sober12months
+
+#changing all names of dataset to lowercase
+names(nsduh2015) <- tolower(names(nsduh2015))
+
+
