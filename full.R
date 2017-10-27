27f022fa42863be20df7c20b853076c468030be4
George Rhodes <gerhodes@uw.edu>
2017-10-27 10:39:28 -0700

    initiallizing Git repository

---
+++ full.R
@@ -0,0 +1,35 @@
+
+
+# Install and load appropriate libraries
+install.packages("dplyr")
+library("dplyr")
+install.packages("readr")
+library("readr")
+install.packages("haven")
+library("haven")
+library("tibble")
+
+
+#load National Survey on Drug Abuse and Mental Health and create object nsduh2015.
+load("/Users/george/Documents/School/UW/SOC321/honors_thesis/honors_thesis/NSDUH-2015-survey-data.rda")
+nsduh2015 <- PUF2015_102016
+
+
+# Who spent time in drug treatment ever? (TX01)		TXEVRRCVD	1=yes
+recieved_treatment <- nsduh2015 %>%
+  select(Variables/columns you want to view) %>%
+  filter(txevrrcvd == 1)
+
+
+# Find out who spent no time getting or drinking alcohol in past 12 months
+# (DRALC01) ALCLOTTM 
+#83= Did not use or used < 6 dys Log asn 117
+#93= Did not use past 12 mo or <6 dys 11935
+sober12months <-
+  
+  #spent time in treatment ever AND hasn't drank in last 12 months
+  #This would be who is sober today. 
+  sober_after_treatment <- recieved_treatment AND sober12months
+
+
+
---
+++ scratchpad.R
@@ -0,0 +1,5 @@
+# scratch script for working through problems. 
+
+#How do I import only specific variables with specific values? 
+#How do I avoid the giant data table and only import relevent data? 
+
